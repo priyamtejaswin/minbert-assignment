5c37f24463c087626595fa327d45bb3c44f883e8
Priyam Tejaswin <priyamtejaswin@gmail.com>
2021-10-11 16:25:39 -0400

    Optimizer Test passed.

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,6 @@
 from typing import Callable, Iterable, Tuple
+from numpy import zeros
+import math
 
 import torch
 from torch.optim import Optimizer
@@ -24,35 +26,62 @@ class AdamW(Optimizer):
             raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
         super().__init__(params, defaults)
+        print("wait")
+        # self.state = []
+        # for gid, group in enumerate(self.param_groups):
+        #     self.state.append([(torch.zeros_like(p), torch.zeros_like(p)) for p in group["params"]])
+
+        self.t = 0
 
     def step(self, closure: Callable = None):
+        self.t += 1
         loss = None
         if closure is not None:
             loss = closure()
 
-        for group in self.param_groups:
-            for p in group["params"]:
+        for gid, group in enumerate(self.param_groups):
+            for pid, p in enumerate(group["params"]):
                 if p.grad is None:
                     continue
                 grad = p.grad.data
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
+                # raise NotImplementedError()
+
 
                 # State should be stored in this dictionary
                 state = self.state[p]
+                if not state:
+                    state['m'] = 0.0
+                    state['v'] = 0.0
+
+                m, v = state['m'], state['v']
+
+
+                b1, b2 = group["betas"]
+                m = b1*m + (1.0 - b1)*grad
+                v = b2*v + (1.0 - b2)*torch.square(grad)
+
+                # self.state[gid][pid] = (m, v)
+                state['m'], state['v'] = m, v
 
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
-
-                # Update first and second moments of the gradients
+                decay = group["weight_decay"]
+                # alt = alpha * math.sqrt(1 - math.pow(b2, self.t)) / (1 - math.pow(b1, self.t))
 
                 # Bias correction
+                # mcap = m / (1 - math.pow(b1, self.t))
+                # vcap = v / (1 - math.pow(b2, self.t))
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
+                alt = alpha * math.sqrt(1 - math.pow(b2, self.t)) / (1 - math.pow(b1, self.t))
 
                 # Update parameters
+                # p.data = p.data - (alt * m / (torch.sqrt(v) + 1e-8) + decay * p.data)
+                p.data = p.data - alt * m / (torch.sqrt(v) + group['eps']) # - alpha*decay*p.data
+                p.data = p.data - alpha*decay*p.data
 
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
